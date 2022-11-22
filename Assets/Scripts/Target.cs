b9aa64aeb8818763719356fdebc8cc5046c428b0
NathanVaughan123 <nathanjvaughan13@gmail.com>
2022-11-22 22:55:31 +1100

    Added particle effects for when objects are hit by sword or bullet.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Target : MonoBehaviour, IDamageable
 {
     [SerializeField] private float health = 100f;
+    [SerializeField] private ParticleSystem hitParticleSystem;
     public void Damage(int damage)
     {
+        GameObject particles = Instantiate(hitParticleSystem.gameObject, this.transform);
+        particles.GetComponent<ParticleSystem>().Play();
+
         this.health -= damage;
         if (health <= 0)
         {
+            particles.gameObject.transform.parent = null;
             Destroy(gameObject);
+            Destroy(particles.gameObject, 2f);
         }
+        Destroy(particles.gameObject, 2f);
     }
 }
